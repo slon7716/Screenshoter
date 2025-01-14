f0a13811a2e8ed23dd8dc878d52c0ba80d46b4ae
Aleksandr <160377@i.ua>
2025-01-14 23:08:25 +0200

    FAQ

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
    $('select').niceSelect();
 });
+
+function installAccordion() {
+   const accordionButton = document.querySelectorAll('.accordion-item__button');
+   accordionButton.forEach(button => {
+      button.addEventListener('click', function (event) {
+         const thisAccordionItem = event.target.closest('.accordion-item');
+         thisAccordionItem.classList.toggle('active');
+      })
+   })
+}
+
+if (document.querySelector('.accordion')) {
+   installAccordion();
+}
